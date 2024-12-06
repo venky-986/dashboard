88e499d4b8b52bf8cd71997c8fc4dd82d7517cd7
Lucas Magalhães <lucas.rodrigues@sou.inteli.edu.br>
2024-12-06 16:50:05 -0300

    feat: add sidebar

--- src/app/(not-logged)/layout.tsx
+++ src/app/(not-logged)/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -22,7 +22,9 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
   return (
     <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
